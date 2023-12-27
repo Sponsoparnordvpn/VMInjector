8a4df4524553615f5499ab05de6e8ed9aac602cb
Sponsoparnordvpn <146754550+Sponsoparnordvpn@users.noreply.github.com>
2023-12-27 07:14:41 +0100

    Create attach.lua

---
+++ attach.lua
@@ -0,0 +1,5 @@
+getkey()
+
+getgenv().key = "land0f0cdbf266e29d3adfae9da32279ee0492ff520340580fb6da99057085b92c5a"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sponsoparnordvpn/VMInjector/main/init_executor.txt",true))()
